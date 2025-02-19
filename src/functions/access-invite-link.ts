bc644df93b45eeff908a96c2322fcffd8abe512d
Julio Gordijo <jgordijo@gmail.com>
2025-02-19 12:10:38 -0300

    feat: add access invite link route and method

---
+++ src/functions/access-invite-link.ts
@@ -0,0 +1,12 @@
+import { subscriptions } from '../drizzle/schema/subscriptions';
+import { redis } from '../redis/client';
+
+interface AccessInviteLinkParams {
+  subscriberId: string;
+}
+
+export async function accessInviteLink({
+  subscriberId,
+}: AccessInviteLinkParams) {
+  await redis.hincrby('referral:access-count', subscriberId, 1);
+}
